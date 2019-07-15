9c7d5ed7dc95678241eb629bca4278f50db9f0c1
ehildenb <everett.hildenbrandt@gmail.com>
2019-07-15 14:25:36 -0500

    cli.js: add riot chat room link

--- cli.js
+++ cli.js
@@ -218,8 +218,9 @@ server.listen(options.port, options.hostname, function(err, result) {
   console.log("*    This uses KEVM as the Ethereum Virtual Machine, NOT EthereumJS-VM !!     *")
   console.log("*                                                                             *")
   console.log("* Please send us feedback!                                                    *")
-  console.log("* http://www.runtimeverification.com                                          *")
-  console.log("* http://www.github.com/runtimeverification                                   *")
+  console.log("* https://www.runtimeverification.com                                          *")
+  console.log("* https://www.github.com/runtimeverification                                   *")
+  console.log("* https://riot.im/app/#/room/#k:matrix.org                                    *")
   console.log("*******************************************************************************")
 });
 
